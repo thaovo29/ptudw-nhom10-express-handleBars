36911db10538837b4d405df35d160cb670656833
Trinh Quoc Huy <47996835+huyquoctrinh@users.noreply.github.com>
2023-05-11 20:42:52 +0700

    Update task2Route.js

--- routes/task2Route.js
+++ routes/task2Route.js
@@ -1,8 +1,26 @@
 const express = require("express")
 const route = express.Router()
+const bodyParser = require('body-parser')
+
+route.use(bodyParser.json());
+route.use(bodyParser.urlencoded({ extended: false }));
+// route.use(express.urlencoded());
 
 route.get('/', (req,res) =>{
     res.render('task2')
+})
+
+route.post('/', (req, res) =>{
+
+    let salary = JSON.stringify(req.body.salary);
+    salary = parseFloat(salary.replace('"',""));
+    var salary10 = salary*0.1.toFixed(2);
+    var salary55 = salary*0.55.toFixed(2);
+    var salary5 = salary*0.05.toFixed(2);
+
+    // console.log(salary10)
+    res.render('task2',{salary10, salary55, salary5})
+    
 })
 
 
